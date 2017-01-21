071239013bf9930cf5fc9d3bbb30e8619e1870a1
codemattcode <codemattcode@gmail.com>
2017-01-21 04:01:39 +0000

    add rest-api plugin plus fake content

--- wp-content/themes/olympos/index.php
+++ wp-content/themes/olympos/index.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-	<h1>THIS IS A WORDPRESS TEST SITE</h1>
-    <p>
-        We ARE going to figure stuff out here...and load plugins and test them also...
-    </p>
+	<div class="white-wrap">
+		<div id="app">
 
+			<router-view></router-view>	
+
+		</div>
+	</div>
 
 
 <?php get_footer(); ?>
